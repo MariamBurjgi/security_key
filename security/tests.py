f39203ee56140ed71ceca4e114248995a216d267
programming <MariamBurjgi>
2024-06-17 19:15:47 +0400

    create class + setup

--- security/tests.py
+++ security/tests.py
@@ -2,3 +2,8 @@ from .models import Channel
 from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
+
+class ChannelAPITest(APITestCase):
+    def setUp(self):
+            self.sender = User.objects.create_user(username='sender', password='password')
+            self.recipient = User.objects.create_user(username='recipient', password='password')
