9602ef40109add57d64269c591170bf72f20ca91
programming <MariamBurjgi>
2024-06-17 19:24:52 +0400

    add test channel create

--- security/tests.py
+++ security/tests.py
@@ -7,3 +7,8 @@ class ChannelAPITest(APITestCase):
     def setUp(self):
             self.sender = User.objects.create_user(username='sender', password='password')
             self.recipient = User.objects.create_user(username='recipient', password='password')
+    def test_channel_creation(self):
+                self.client.login(username='sender', password='password')
+                response = self.client.post('/api/channels/', {'recipient_user': 'recipient'})
+                self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+                self.assertEqual(Channel.objects.count(), 1)
